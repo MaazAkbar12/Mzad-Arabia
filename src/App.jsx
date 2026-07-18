e9e556a070edfe9c8eb3b86eadc17fcbd65731c6
Dell <https://github.com/MaazAkbar8/Die-Roll-Dise.git>
2026-07-18 06:54:56 -0700

    fixed admin 404 route issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // BrowserRouter ko HashRouter se badla
 
 import Header from "./components/header/header";
 import Footer from "./components/Footer/Footer";
-
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PostAd from "./pages/PostAd";
 
-
 import AdminRoutes from "./admin/routes/AdminRoutes";
 
-
-
 function App() {
-
-
   return (
-
-
-    <BrowserRouter basename="/Mzad-Arabia">
-
-
+    // HashRouter ke saath basename ki zaroorat nahi hoti, yeh automatic handle ho jata hai
+    <Router>
       <Routes>
-
-
         {/* Website Routes */}
-
         <Route
-
           path="/*"
-
           element={
-
             <>
-
               <Header />
-
-
               <Routes>
-
-
                 <Route path="/" element={<Home />} />
-
                 <Route path="/login" element={<Login />} />
-
                 <Route path="/register" element={<Register />} />
-
                 <Route path="/post-ad" element={<PostAd />} />
-
-
               </Routes>
-
-
               <Footer />
-
-
             </>
-
           }
-
         />
-
-
 
         {/* Admin Routes */}
-
         <Route
-
           path="/admin/*"
-
           element={<AdminRoutes />}
-
         />
-
-
       </Routes>
-
-
-    </BrowserRouter>
-
-
+    </Router>
   );
-
-
 }
-
 
 export default App;
